b6b9c9e5caaaa3d1ce5a2158d0884915d03c482f
kipyeg <MY_NAME@example.com>
2023-10-09 17:19:00 +0300

    create header component

--- clients/src/components/Header.js
+++ clients/src/components/Header.js
@@ -1,12 +1,40 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
 function Header() {
   return (
-    <header>
-      <h1 className="font-bold">
-        <span className="text-blue-800">ptah</span>
-        <span className="text-red-900">clyde</span>
-      </h1>
+    <header className="bg-slate-500 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-blue-800">ptah</span>
+            <span className="text-red-900">clyde</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-1 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent focus:outline-none w-150 sm:w-40"
+          />
+          <FaSearch className="text-slate-600" />
+        </form>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className=" hidden sm:inline text-black-400 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className=" hidden sm:inline text-black-400 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/signin">
+            <li className="text-black-400 hover:underline">Sign-in</li>
+          </Link>
+        </ul>
+      </div>
     </header>
   );
 }
